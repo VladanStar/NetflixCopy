fcb57ec1699189e42b9a0ca5bebdcfdc6c522353
VladanStar <vladancupric@gmail.com>
2023-02-26 22:37:44 +0100

    Add

--- NetflixCopy/src/app/pages/home/home.component.ts
+++ NetflixCopy/src/app/pages/home/home.component.ts
@@ -13,9 +13,26 @@ constructor( private service:MovieApiServiceService){}
 
 bannerResult:any=[];
 trendingMovieResult:any=[]
+
+actionMovieResult: any = [];
+adventureMovieResult: any = [];
+animationMovieResult: any = [];
+comedyMovieResult: any = [];
+documentaryMovieResult: any = [];
+sciencefictionMovieResult: any = [];
+thrillerMovieResult: any = [];
+
+
 ngOnInit():void{
 this.bannerData();
 this.trendingData();
+this.actionMovie();
+this.adventureMovie();
+this.comedyMovie();
+this.animationMovie();
+this.documentaryMovie();
+this.sciencefictionMovie();
+this.thrillerMovie();
 
 }
 //bannerData
@@ -32,6 +49,60 @@ console.log(result, "trendingresult#");
 this.trendingMovieResult=result.results;
 console.log(this.trendingMovieResult)
 })
+}
+ // action
+ actionMovie() {
+  this.service.fetchActionMovies().subscribe((result) => {
+    this.actionMovieResult = result.results;
+  });
+}
+
+
+
+
+// adventure
+adventureMovie() {
+  this.service.fetchAdventureMovies().subscribe((result) => {
+    this.adventureMovieResult = result.results;
+  });
+}
+
+
+// animation
+animationMovie() {
+  this.service.fetchAnimationMovies().subscribe((result) => {
+    this.animationMovieResult = result.results;
+  });
+}
+
+
+// comedy
+comedyMovie() {
+  this.service.fetchComedyMovies().subscribe((result) => {
+    this.comedyMovieResult = result.results;
+  });
+}
+
+// documentary
+documentaryMovie() {
+  this.service.fetchDocumentaryMovies().subscribe((result) => {
+    this.documentaryMovieResult = result.results;
+  });
+}
+
+
+// science-fiction
+sciencefictionMovie() {
+  this.service.fetchScienceFictionMovies().subscribe((result) => {
+    this.sciencefictionMovieResult = result.results;
+  });
+}
+
+// thriller
+thrillerMovie() {
+  this.service.fetchThrillerMovies().subscribe((result) => {
+    this.thrillerMovieResult = result.results;
+  });
 }
 
 }
--- NetflixCopy/src/app/service/movie-api-service.service.ts
+++ NetflixCopy/src/app/service/movie-api-service.service.ts
@@ -53,4 +53,41 @@ getMovieCast(data:any):Observable<any>
   console.log(data, "movie#")
   return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`)
 }
+
+// action
+fetchActionMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=28`);
+}
+
+// adventure
+fetchAdventureMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=12`);
+}
+
+// animation
+fetchAnimationMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=16`);
+}
+
+// comedy
+fetchComedyMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=35`);
+}
+
+// documentary
+fetchDocumentaryMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=99`);
+}
+
+// science-fiction:878
+
+fetchScienceFictionMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=878`);
+}
+
+// thriller:53
+fetchThrillerMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=53`);
+}
+
 }
