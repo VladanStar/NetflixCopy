b2c8e87dc3f12082129ddd7439aeecdd20414737
VladanStar <vladancupric@gmail.com>
2023-02-26 13:02:10 +0100

    Add

--- NetflixCopy/src/app/pages/home/home.component.ts
+++ NetflixCopy/src/app/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+constructor( private service:MovieApiServiceService){}
+
+ngOnInit():void{
+this.bannerData();
+
+}
+//bannerData
+bannerData(){
+this.service.bannerApiData().subscribe((result) =>{
+console.log(result,"bannerresult#")
+})
+}
 
 }
--- NetflixCopy/src/app/service/movie-api-service.service.ts
+++ NetflixCopy/src/app/service/movie-api-service.service.ts
@@ -10,14 +10,15 @@ export class MovieApiServiceService {
 
   constructor( private http:HttpClient) {}
 
-baseurl:any="https://api:themoviedb.org/3";
-apikey:any="7a42535d07bab5cf8947d3b0291eb0d3";
+baseurl="https://api.themoviedb.org/3";
+apikey="7a42535d07bab5cf8947d3b0291eb0d3";
+//08cc33bd5ae3a747598ce2ad84376e66
 
 
 //bannerapiData
 bannerApiData():Observable<any>
 {
-return this.http.get(`$(this.baseurl}/trending/all/week?api_key=${this.apikey})`)
+return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`)
 }
 //bannerapi='${this.baseurl}/trending/all/week?api_key=${this.apikey}'
 
