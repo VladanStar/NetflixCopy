f6aba67da7e9b0993bbbd6ee3a9585f9d7f3eedb
VladanStar <vladancupric@gmail.com>
2023-02-26 11:27:03 +0100

    add service in app.module.ts

--- NetflixCopy/src/app/app.module.ts
+++ NetflixCopy/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
-
+import { MovieApiServiceService } from './service/movie-api-service.service';
 
 @NgModule({
   declarations: [
@@ -14,10 +14,9 @@ import { MovieDetailsComponent } from './pages/movie-details/movie-details.compo
     HomeComponent,
     SearchComponent,
     MovieDetailsComponent,
-
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
