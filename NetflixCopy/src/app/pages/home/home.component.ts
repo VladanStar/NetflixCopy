76cfe166ef174bd079564588ae4f17b8af279620
VladanStar <vladancupric@gmail.com>
2023-02-26 13:24:10 +0100

    Add

--- NetflixCopy/src/app/pages/home/home.component.ts
+++ NetflixCopy/src/app/pages/home/home.component.ts
@@ -10,6 +10,8 @@ import { MovieApiServiceService } from 'src/app/service/movie-api-service.servic
 export class HomeComponent implements OnInit {
 constructor( private service:MovieApiServiceService){}
 
+
+bannerResult:any=[];
 ngOnInit():void{
 this.bannerData();
 
@@ -17,7 +19,9 @@ this.bannerData();
 //bannerData
 bannerData(){
 this.service.bannerApiData().subscribe((result) =>{
-console.log(result,"bannerresult#")
+console.log(result,"bannerresult#");
+this.bannerResult=result.results;
+console.log(this.bannerResult)
 })
 }
 
