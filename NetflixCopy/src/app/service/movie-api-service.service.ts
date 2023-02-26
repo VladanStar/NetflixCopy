1ce640b48af18000afce3a1f70b9361ba42ab22f
VladanStar <vladancupric@gmail.com>
2023-02-26 11:57:05 +0100

    Service apikey baseurl

--- NetflixCopy/src/app/service/movie-api-service.service.ts
+++ NetflixCopy/src/app/service/movie-api-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiServiceService {
 
-  constructor() { }
+
+
+  constructor( private http:HttpClient) {}
+
+baseurl:any="https://api:themoviedb.org/3";
+apikey:any="7a42535d07bab5cf8947d3b0291eb0d3";
+
+
+//bannerapiData
+bannerApiData():Observable<any>
+{
+return this.http.get(`$(this.baseurl}/trending/all/week?api_key=${this.apikey})`)
+}
+//bannerapi='${this.baseurl}/trending/all/week?api_key=${this.apikey}'
+
 }
